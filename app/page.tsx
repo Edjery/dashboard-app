238c32eebf95e0e710b1aa7752fbe501860ce321
EJ <edisgumbao@gmail.com>
2024-03-21 13:20:34 +0800

    Add: MUI Sample Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import { Box, Typography } from "@mui/material";
+"use client";
+
+import { Box, CssBaseline } from "@mui/material";
+import HomeContent from "./components/HomeContent";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
-    <Box>
-      <Typography>Henlo</Typography>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <NavBar />
+      <HomeContent />
     </Box>
   );
 }
