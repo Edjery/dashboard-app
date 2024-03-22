101c19983e4ebe60caf223a059aae7a5528a3383
EJ <edisgumbao@gmail.com>
2024-03-22 16:19:10 +0800

    Update Content to show only when authenticated

--- app/components/HomeContent.tsx
+++ app/components/HomeContent.tsx
@@ -1,9 +1,19 @@
-import { Box, Container, Grid, Paper, Toolbar } from "@mui/material";
+import {
+  Box,
+  Container,
+  Grid,
+  Paper,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useSession } from "next-auth/react";
 import Chart from "./Chart";
 import Deposits from "./Deposits";
 import Orders from "./Orders";
 
 const HomeContent = () => {
+  const { status } = useSession();
+
   return (
     <Box
       component="main"
@@ -19,37 +29,45 @@ const HomeContent = () => {
     >
       <Toolbar />
       <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={8} lg={9}>
-            <Paper
-              sx={{
-                p: 2,
-                display: "flex",
-                flexDirection: "column",
-                height: 240,
-              }}
-            >
-              <Chart />
-            </Paper>
-          </Grid>
-          <Grid item xs={12} md={4} lg={3}>
-            <Paper
-              sx={{
-                p: 2,
-                display: "flex",
-                flexDirection: "column",
-                height: 240,
-              }}
-            >
-              <Deposits />
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-              <Orders />
-            </Paper>
-          </Grid>
-        </Grid>
+        {status === "authenticated" ? (
+          <>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={8} lg={9}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: "flex",
+                    flexDirection: "column",
+                    height: 240,
+                  }}
+                >
+                  <Chart />
+                </Paper>
+              </Grid>
+              <Grid item xs={12} md={4} lg={3}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: "flex",
+                    flexDirection: "column",
+                    height: 240,
+                  }}
+                >
+                  <Deposits />
+                </Paper>
+              </Grid>
+              <Grid item xs={12}>
+                <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+                  <Orders />
+                </Paper>
+              </Grid>
+            </Grid>
+          </>
+        ) : (
+          <Typography variant="h4" fontWeight="bold">
+            You must be logged in to see the data
+          </Typography>
+        )}
       </Container>
     </Box>
   );
